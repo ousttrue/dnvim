73a751e3287e96ce58f129ef8e7729450d5a4bed
ousttrue <ousttrue@gmail.com>
2018-08-09 10:45:55 +0900

    MsgPack test

--- main.cpp
+++ main.cpp
@@ -3,17 +3,36 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#include <msgpackpp.h>
 
 
 int main(int argc, char **argv)
 {
 	std::cout << std::endl << "Example 5 - demonstrates Process::try_get_exit_status" << std::endl;
 
-	TinyProcessLib::Process process1("nvim.exe --embed", "", [](const char *bytes, size_t n) {
-		std::cout << "Output from stdout: " << std::string(bytes, n);
-	});
-	auto exit_status = process1.get_exit_status();
+	TinyProcessLib::Process process1("nvim --embed", "", [](const char *bytes, size_t n) {
+
+		auto u = msgpackpp::parser((const uint8_t*)bytes, n);
+		std::cout << "Output from stdout: " << u.to_json() << std::endl;
+
+	}, nullptr, true);
+
+	msgpackpp::packer packer;
+	packer.pack_array(4);
+	packer.pack_integer(0);
+	packer.pack_integer(1);
+	packer.pack_str("nvim_ui_attach");
+	packer.pack_array(3);
+	packer.pack_integer(80);
+	packer.pack_integer(56);
+	packer.pack_map(0);
+
+	// std::vector<std::uint8_t>
+	auto p = packer.get_payload();
+	process1.write((const char*)p.data(), p.size());
+
 	std::this_thread::sleep_for(std::chrono::seconds(2));
+	auto exit_status = process1.get_exit_status();
 
 	return exit_status;
 }
