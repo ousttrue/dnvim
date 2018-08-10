abb9d3ff70505f9c0a2263c548cca9df03669149
ousttrue <ousttrue@gmail.com>
2018-08-11 03:04:29 +0900

    Added Cursor and Cell

--- dnvim/grid.cpp
+++ dnvim/grid.cpp
@@ -1,42 +1,67 @@
 #include "grid.h"
+#include <plog/Log.h>
 
 
-void Grid::option_set(std::string key, bool enable) {
-
+void Grid::option_set(std::string key, bool enable) 
+{
 }
 
-void Grid::default_colors_set(int i0, int i1, int i2, int i3, int i4) {
 
-}
-
-void Grid::update_fg(int g) {
-}
-
-void Grid::update_bg(int g) {
-}
-
-void Grid::update_sp(int g) {
-}
-
-void Grid::resize(int cols, int rows) {
-
-}
-
-void Grid::clear() {
-
-}
-
-void Grid::cursor_goto(int col, int row) {
-
-}
-
-void Grid::highlight_set(Highlight hl) {
-
-}
-
-void Grid::put(std::string str)
+void Grid::default_colors_set(int rgb_fg, 
+	int rgb_bg, int rgb_sp,
+	int cterm_fg, int cterm_bg) 
 {
 
+}
+
+void Grid::update_fg(int color) 
+{
+}
+
+void Grid::update_bg(int color) 
+{
+}
+
+void Grid::update_sp(int color) 
+{
+}
+
+void Grid::resize(int cols, int rows) 
+{
+	LOGD << "resize: " << cols << ", " << rows;
+	m_cells.resize(cols * rows);
+	m_cols = cols;
+}
+
+void Grid::clear()
+{
+	LOGD << "clear";
+	for (auto &cell : m_cells)
+	{
+		cell.code = 0; // space
+	}
+}
+
+void Grid::cursor_goto(int col, int row) 
+{
+	LOGD << "cursor_goto: " << col << ", " << row;
+	m_cursor.col = col;
+	m_cursor.row = row;
+}
+
+void Grid::highlight_set(Highlight hl) 
+{
+}
+
+void Grid::put(std::string text)
+{
+	LOGD << "put: '" << text << "'";
+	auto cell = get_current_cell();
+	for (char c : text)
+	{
+		cell->code = c;
+		++cell;
+	}
 }
 
 void Grid::mode_info_set(bool cursor_style_enabled, msgpackpp::parser map)
@@ -48,4 +73,3 @@ void Grid::mode_change(std::string mode, int value)
 {
 
 }
-
--- dnvim/grid.h
+++ dnvim/grid.h
@@ -1,4 +1,6 @@
 #include <string>
+#include <vector>
+#include <memory>
 #include <msgpackpp.h>
 
 
@@ -10,23 +12,47 @@ struct Highlight
 MPPP_MAP_SERIALIZER(Highlight, 2, bold, foreground);
 
 
+struct Cell
+{
+	char32_t code;
+};
+
+
+struct Cursor
+{
+	int col=0;
+	int row=0;
+};
+
+
 ///
 /// https://github.com/neovim/neovim/blob/master/runtime/doc/ui.txt
 ///
 class Grid
 {
+	std::vector<Cell> m_cells;
+	int m_cols=0;
+	Cursor m_cursor;
+
+	Cell* get_current_cell()
+	{
+		auto index = m_cursor.row * m_cols + m_cursor.col;
+		return &m_cells[index];
+	}
+
 public:
 	void option_set(std::string key, bool enable);
-	void default_colors_set(int i0, int i1, int i2, int i3, int i4);
-	void update_fg(int g);
-	void update_bg(int g);
-	void update_sp(int g);
+	void default_colors_set(
+		int rgb_fg, int rgb_bg, int rgb_sp, 
+		int cterm_fg, int cterm_bg);
+	void update_fg(int color);
+	void update_bg(int color);
+	void update_sp(int color);
 	void resize(int cols, int rows);
 	void clear();
 	void cursor_goto(int col, int row);
 	void highlight_set(Highlight hl);
-	void put(std::string str);
+	void put(std::string text);
 	void mode_info_set(bool cursor_style_enabled, msgpackpp::parser map);
 	void mode_change(std::string mode, int value);
 };
-
