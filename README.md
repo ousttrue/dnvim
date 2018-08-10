e5073c2d2df5fa67a183e94555caca3edc44c9a7
ousttrue <ousttrue@gmail.com>
2018-08-10 16:10:29 +0900

    Added d3d window

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # dnvim(Direct nvim)
 
-nvim frontend by Direct2D
+nvim frontend by D3D
 
+# dependencies
+
+* https://gitlab.com/eidheim/tiny-process-library
 * https://github.com/ousttrue/msgpackpp
+* https://github.com/SergiusTheBest/plog
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "D3D11Manager.h"
+#include "resource.h"
+
 #include <process.hpp>
 #include <string>
 #include <chrono>
@@ -82,7 +85,7 @@ private:
 
 	bool consume()
 	{
-		auto msg = msgpackpp::parser(m_buffer.data(), m_buffer.size());
+		auto msg = msgpackpp::parser(m_buffer.data(), static_cast<int>(m_buffer.size()));
 		auto d = msg.consumed_size();
 		m_buffer.erase(m_buffer.begin(), m_buffer.begin() + d);
 
@@ -195,7 +198,10 @@ public:
 };
 
 
-#define LAUNCH_COMMAND "nvim --embed"
+#define LAUNCH_COMMAND L"nvim --embed"
+#define WINDOW_CLASS L"dnvim_ui"
+#define WINDOW_TITLE L"dnvim"
+auto textureFile = L"../MinTriangle/texture.png";
 
 
 class NVim
@@ -223,14 +229,23 @@ public:
 #undef DISPACHER_ADD_METHOD
 	}
 
-	void Launch(const char *cmd)
+	~NVim()
+	{
+		if (m_process) {
+			m_process->kill();
+			m_process->get_exit_status();
+			m_process = nullptr;
+		}
+	}
+
+	void Launch(const wchar_t *cmd)
 	{
 		auto callback = [this](const char *bytes, size_t n) {
 
 			this->m_dispatcher.push_bytes(bytes, n);
 
 		};
-		m_process = std::shared_ptr<TinyProcessLib::Process>(new TinyProcessLib::Process(cmd, "", callback, nullptr, true));
+		m_process = std::shared_ptr<TinyProcessLib::Process>(new TinyProcessLib::Process(cmd, L"", callback, nullptr, true));
 
 		msgpackpp::packer packer;
 		packer.pack_array(4);
@@ -247,12 +262,107 @@ public:
 		m_process->write((const char*)p.data(), p.size());
 	}
 
-	int GetExitStatus()
+	bool GetExitStatus(int *_exit_status)
 	{
+		int exit_status;
 		//std::this_thread::sleep_for(std::chrono::seconds(2));
-		return m_process->get_exit_status();
+		if (m_process->try_get_exit_status(exit_status)) {
+			if (_exit_status)*_exit_status = exit_status;
+			return true;
+		}
+
+		return false;
 	}
 };
+
+
+static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	auto d3d = (D3D11Manager*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		auto d3d = (D3D11Manager*)((LPCREATESTRUCT)lParam)->lpCreateParams;
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)d3d);
+		break;
+	}
+
+	case WM_ERASEBKGND:
+		return 0;
+
+	case WM_SIZE:
+		d3d->Resize(LOWORD(wParam), HIWORD(wParam));
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+	}
+	return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+
+static HWND CreateUIWindow(HINSTANCE hInstance, int nCmdShow, D3D11Manager &d3d11)
+{
+	{
+		WNDCLASSEX wcex;
+		wcex.cbSize = sizeof(WNDCLASSEX);
+		wcex.style = CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc = WndProc;
+		wcex.cbClsExtra = 0;
+		wcex.cbWndExtra = 0;
+		wcex.hInstance = hInstance;
+		wcex.hIcon = NULL;
+		wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+		wcex.lpszMenuName = NULL;//MAKEINTRESOURCE(IDC_D3D11SAMPLE);
+		wcex.lpszClassName = WINDOW_CLASS;
+		wcex.hIconSm = NULL;
+		RegisterClassEx(&wcex);
+	}
+	HWND hWnd = CreateWindow(WINDOW_CLASS, WINDOW_TITLE, WS_OVERLAPPEDWINDOW
+		, CW_USEDEFAULT, CW_USEDEFAULT
+		, 320, 320
+		, NULL, NULL, hInstance, &d3d11);
+	if (!hWnd)
+	{
+		return nullptr;
+	}
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return hWnd;
+}
+
+
+static std::string GetShader(HINSTANCE hInst)
+{
+	HRSRC hRes = FindResource(hInst, MAKEINTRESOURCE(ID_SHADERSOURCE), L"SHADERSOURCE");
+	if (!hRes) {
+		return "";
+	}
+	HGLOBAL hMem = LoadResource(hInst, hRes);
+	DWORD size = SizeofResource(hInst, hRes);
+	char* resText = (char*)LockResource(hMem);
+	char* text = (char*)malloc(size + 1);
+	memcpy(text, resText, size);
+	text[size] = 0;
+	std::string s(text);
+	free(text);
+	FreeResource(hMem);
+	return s;
+}
 
 
 int WINAPI WinMain(
@@ -279,8 +389,41 @@ int WINAPI WinMain(
 	nvim.Launch(LAUNCH_COMMAND);
 
 	LOGI << "create window";
+	D3D11Manager d3d11;
+	auto hWnd = CreateUIWindow(hInstance, nCmdShow, d3d11);
 
-	auto exit_status = nvim.GetExitStatus();
+	auto shaderSource = GetShader(hInstance);
+	if (shaderSource.empty()) {
+		return 5;
+	}
+
+	// d3d
+	if (!d3d11.Initialize(hWnd, shaderSource, textureFile)) {
+		return 2;
+	}
+
+	// main loop
+	MSG msg;
+	int exit_status;
+	while (true)
+	{
+		if (nvim.GetExitStatus(&exit_status)) {
+			break;
+		}
+
+		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		{
+			if (!GetMessage(&msg, NULL, 0, 0)) {
+				exit_status = (int)msg.wParam;
+				break;
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			d3d11.Render();
+		}
+	}
 
 	return exit_status;
 }
