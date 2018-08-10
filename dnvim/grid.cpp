974269c5e37914d29686baf6a14e0f11565815eb
ousttrue <ousttrue@gmail.com>
2018-08-11 01:16:02 +0900

    Separated grid

---
+++ dnvim/grid.cpp
@@ -0,0 +1,51 @@
+#include "grid.h"
+
+
+void Grid::option_set(std::string key, bool enable) {
+
+}
+
+void Grid::default_colors_set(int i0, int i1, int i2, int i3, int i4) {
+
+}
+
+void Grid::update_fg(int g) {
+}
+
+void Grid::update_bg(int g) {
+}
+
+void Grid::update_sp(int g) {
+}
+
+void Grid::resize(int cols, int rows) {
+
+}
+
+void Grid::clear() {
+
+}
+
+void Grid::cursor_goto(int col, int row) {
+
+}
+
+void Grid::highlight_set(Highlight hl) {
+
+}
+
+void Grid::put(std::string str)
+{
+
+}
+
+void Grid::mode_info_set(bool cursor_style_enabled, msgpackpp::parser map)
+{
+    //std::cout << "mode_info_set: " << map;
+}
+
+void Grid::mode_change(std::string mode, int value)
+{
+
+}
+
--- dnvim/nvimlauncher.cpp
+++ dnvim/nvimlauncher.cpp
@@ -1,78 +1,7 @@
 #include "nvimlauncher.h"
 #include "dispatcher.h"
+#include "grid.h"
 #include <process.hpp>
-#include <string>
-#include <chrono>
-#include <thread>
-#include <msgpackpp.h>
-#include <vector>
-#include <functional>
-#include <plog/Log.h>
-
-
-
-struct Highlight
-{
-	bool bold = false;
-	int foreground = 255;
-};
-MPPP_MAP_SERIALIZER(Highlight, 2, bold, foreground);
-
-
-///
-/// https://github.com/neovim/neovim/blob/master/runtime/doc/ui.txt
-///
-class Grid
-{
-public:
-	void option_set(std::string key, bool enable) {
-
-	}
-
-	void default_colors_set(int i0, int i1, int i2, int i3, int i4) {
-
-	}
-
-	void update_fg(int g) {
-	}
-
-	void update_bg(int g) {
-	}
-
-	void update_sp(int g) {
-	}
-
-	void resize(int cols, int rows) {
-
-	}
-
-	void clear() {
-
-	}
-
-	void cursor_goto(int col, int row) {
-
-	}
-
-	void highlight_set(Highlight hl) {
-
-	}
-
-	void put(std::string str)
-	{
-
-	}
-
-	void mode_info_set(bool cursor_style_enabled, msgpackpp::parser map)
-	{
-		//std::cout << "mode_info_set: " << map;
-	}
-
-	void mode_change(std::string mode, int value)
-	{
-
-	}
-};
 
 
 class NVimImpl
